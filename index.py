cf1301811488641deba4df305288a288b38fc695
Nicole Baram <nicolebaram@Nicoles-MacBook-Pro.local>
2019-04-25 15:39:48 -0400

    to do list!

---
+++ index.py
@@ -0,0 +1,12 @@
+def main():
+    ### 1. generate two prime #'s
+    ###     a. have a primality test
+    ### 2. Times them together!!
+    ### 3. Select e s.t. gcd(e, p-1,q-1) = 1
+    ###     a. have a gcd function
+    ###     b. how to get e??
+    ### 4. Calculate d (multiplicative inverse)
+    ###     a. how to do that?
+### ---
+    ### 5. convert strings to nums and back again
+main()
