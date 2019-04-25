6935fe8684958c5eb6f28b00e79139498e41fd43
Nicole Baram <nicolebaram@Nicoles-MacBook-Pro.local>
2019-04-25 17:46:01 -0400

    added kiwakos code back omg git

--- index.py
+++ index.py
@@ -1,4 +1,39 @@
+import random
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from nprime.pyprime import miller_rabin
+##pip install nprime
+
+## 3a. implementing gcd with Euclid's algorithm
+def gcd(x,y):
+	if y == 0:
+		return x
+	else:
+		return gcd(y,x%y)
+
+
 def main():
+    while True:
+        p = random.randint(65537, 6553700)
+        if miller_rabin(p):
+            break
+    while True:
+        q = random.randint(65537, 6553700)
+        if miller_rabin(q):
+            break
+    print(p, q)
+
+
+    ### 3b. refer to https://crypto.stackexchange.com/questions/3110/impacts-of-not-using-rsa-exponent-of-65537
+    e = 65537
+    ## for security, padding seems to be more important than the size of public exponent?
+    ## we can test the algorithm with small e if it takes too long time to compute.
+    e_test = 3
+
+    ### 4. multiplicative inverse
+
+
     ### 1. generate two prime #'s (nicole)
     ###     a. have a primality test
     ### 2. Times them together!!
@@ -7,6 +42,7 @@ def main():
     ###     b. how to get e??
     ### 4. Calculate d (multiplicative inverse)
     ###     a. how to do that?
-### ---
+    ### ---
     ### 5. convert strings to nums and back again
+    ### 6. Make it secure with padding && masking etc etc etc
 main()
