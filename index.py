ae4b40b8205569484c79520f725771b0d88ca8d8
pythagoraswitch <kiwako.sakamoto@gmail.com>
2019-04-25 17:02:36 -0400

    added step 3

--- index.py
+++ index.py
@@ -1,3 +1,25 @@
+
+
+
+
+
+### 3a. implementing gcd with Euclid's algorithm
+def gcd(x,y):
+	if y == 0:
+		return x
+	else:
+		return gcd(y,x%y)
+
+### 3b. refer to https://crypto.stackexchange.com/questions/3110/impacts-of-not-using-rsa-exponent-of-65537
+e = 65537
+## for security, padding seems to be more important than the size of public exponent? 
+## we can test the algorithm with small e if it takes too long time to compute.
+e_test = 3
+
+### 4. multiplicative inverse
+
+
+
 def main():
     ### 1. generate two prime #'s (nicole)
     ###     a. have a primality test
