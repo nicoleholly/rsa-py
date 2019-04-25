5e9535ec521481662d44dec3836b20b42e57e26f
Nicole Baram <nicolebaram@Nicoles-MacBook-Pro.local>
2019-04-25 17:37:46 -0400

    using a library lmao

--- index.py
+++ index.py
@@ -1,4 +1,20 @@
+import random
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from nprime.pyprime import miller_rabin
+##pip install nprime
+
 def main():
+    while True:
+        p = random.randint(65537, 6553700)
+        if miller_rabin(p):
+            break
+    while True:
+        q = random.randint(65537, 6553700)
+        if miller_rabin(q):
+            break
+    print(p, q)
     ### 1. generate two prime #'s (nicole)
     ###     a. have a primality test
     ### 2. Times them together!!
@@ -7,6 +23,7 @@ def main():
     ###     b. how to get e??
     ### 4. Calculate d (multiplicative inverse)
     ###     a. how to do that?
-### ---
+    ### ---
     ### 5. convert strings to nums and back again
+    ### 6. Make it secure with padding && masking etc etc etc
 main()
